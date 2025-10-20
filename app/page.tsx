19567f7736f82acf8200c47d6a814516b77710ae
Shamim Ahmed <shamimeforb@gmail.com>
2025-10-20 14:06:05 +0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
-// Disable caching for this page
+// Disable caching for this pages
 export const revalidate = 0
 
 export default function HomePage() {
